906c7543cb817b1e3c391e6837da0491e96f446c
Jordan Mann <jordaloo@gmail.com>
2020-01-22 17:05:20 +0000

    ?

---
+++ day2/Day2-2.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class Day2 {
+    private static String operate(int pos, String command){
+    	ArrayList<Integer> cmds = new ArrayList<>();
+    	for (String s : command.split(",")) {
+    		cmds.add(Integer.parseInt(s));
+    	}
+    	if (pos == cmds.size()) {
+    		return command;
+    	}
+    	else if (cmds.get(pos) == 99) {
+    		return command;
+    	}
+    	else if (cmds.get(pos) == 1) {
+    		System.out.printf("adding %d and %d%n", cmds.get(cmds.get(pos+1)), cmds.get(cmds.get(pos+2)));
+    		cmds.set(cmds.get(pos+3), cmds.get(cmds.get(pos+1)) + cmds.get(cmds.get(pos+2)));
+    		pos += 4;
+    	}
+    	else if (cmds.get(pos) == 2) {
+    		System.out.printf("multiplying %d and %d%n", cmds.get(cmds.get(pos+1)), cmds.get(cmds.get(pos+2)));
+    		cmds.set(cmds.get(pos+3), cmds.get(cmds.get(pos+1)) * cmds.get(cmds.get(pos+2)));
+    		pos += 4;
+    	}
+    	String newCmd = "";
+    	for (Integer d : cmds) {
+    		newCmd += d.toString();
+    		newCmd += ",";
+    	}
+    	newCmd = newCmd.substring(0,newCmd.length() - 1);
+    	System.out.println(newCmd);
+    	return operate(pos, newCmd);
+    }
+    private static int operate2(String command){
+    	ArrayList<Integer> cmds = new ArrayList<>();
+    	for (String s : command.split(",")) {
+    		cmds.add(Integer.parseInt(s));
+    	}
+        for (int i = 0; i < cmds.size(); i++){}
+        return 0;
+    }
+    public static void main(String[] args){
+        try {
+            if (args.length != 1){
+                System.err.println("Expected filename argument");
+                return;
+            }
+            Scanner fscan = new Scanner(new File(args[0]));
+            String input = "";
+            while (fscan.hasNextLine()){
+                input += fscan.nextLine();
+            }
+            System.out.printf("Output is: %s%n", operate2(input));
+        }
+        catch (FileNotFoundException e){
+            System.err.println("File not found.");
+        }
+    }
+}
